7ee3651091e5b942f9141e3bae8fb65b8bab4776
bezbiletniy <a.borodin.ca@gmail.com>
2022-06-26 00:08:37 +0300

    Reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
-        public int calculate(int cost) {
-            int rub = 20;
-            int result = cost/rub;
-            return result;
+    public int calculate(int cost) {
+        int rub = 20;
+        int result = cost / rub;
+        return result;
     }
 }
